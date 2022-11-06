f01c910015842b3463080fa262e633e22fe4cc1c
Evharisto <sv.lisitskaya@mail.ru>
2022-11-06 10:07:56 +0300

    add repo

---
+++ src/test/java/guru/qa/tests/TestBase.java
@@ -0,0 +1,37 @@
+package guru.qa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import guru.qa.utils.AddAttachments;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        //     Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        if (System.getProperty("remote") != null) {
+            Configuration.remote = System.getProperty("remote");
+        }
+    }
+
+    @AfterEach
+    void addAttachments() {
+        AddAttachments.screenshotAs("Last screenshot");
+        AddAttachments.addVideo();
+        AddAttachments.pageSource();
+        AddAttachments.browserConsoleLogs();
+    }
+
+}
---
+++ src/test/java/guru/qa/tests/TextBoxTests.java
@@ -0,0 +1,57 @@
+package guru.qa.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        //    Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("ivanivanov@gmail.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("8800555353");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("1971");
+        $(".react-datepicker__day--019").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("image.jpg");
+        $("#currentAddress").setValue("Calgary");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $$(".table-responsive").findBy(text("Student Name")).shouldHave(text("Ivan Ivanov"));
+        $$(".table-responsive").findBy(text("Student Email")).shouldHave(text("ivanivanov@gmail.com"));
+        $$(".table-responsive").findBy(text("Gender")).shouldHave(text("Male"));
+        $$(".table-responsive").findBy(text("Mobile")).shouldHave(text("8800555353"));
+        $$(".table-responsive").findBy(text("Date of Birth")).shouldHave(text("19 February,1971"));
+        $$(".table-responsive").findBy(text("Subjects")).shouldHave(text("Computer Science"));
+        $$(".table-responsive").findBy(text("Hobbies")).shouldHave(text("Sports"));
+        $$(".table-responsive").findBy(text("Picture")).shouldHave(text("image.jpg"));
+        $$(".table-responsive").findBy(text("Address")).shouldHave(text("Calgary"));
+        $$(".table-responsive").findBy(text("State and City")).shouldHave(text("NCR Delhi"));
+
+
+    }
+
+}
